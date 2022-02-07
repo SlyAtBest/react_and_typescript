7ed3443e3c7eff63f4b77d70cb38d213578fe4f9
matd <slyatbest@gmail.com>
2022-02-07 10:21:18 +0000

    Mass commit of work completed as part of section 1-12.

---
+++ features/generics.ts
@@ -0,0 +1,74 @@
+class ArrayOfNumbers {
+  constructor(public collection: number[]) {}
+
+  get(index: number): number {
+    return this.collection[index];
+  }
+}
+
+class ArrayOfString {
+  constructor(public collection: string[]) {}
+
+  get(index: number): string {
+    return this.collection[index];
+  }
+}
+
+class ArrayOfAnything<T> {
+  constructor(public collection: T[]) {}
+
+  get(index: number): T {
+    return this.collection[index];
+  }
+}
+
+const arr = new ArrayOfAnything<string>(['a', 'b', 'c']);
+
+// Example of generics with functions
+
+function printStrings(arr: string[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+function printNumbers(arr: number[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+function printAnything<T>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+printAnything<string>(['a', 'b', 'c']);
+
+// Generic Constraints
+
+class Scooter {
+  print() {
+    console.log('I am a scooter');
+  }
+}
+
+class House {
+  print() {
+    console.log('I am a house');
+  }
+}
+
+interface Printable {
+  print(): void;
+}
+
+function printHousesOrScooters<T extends Printable>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].print();
+  }
+}
+
+printHousesOrScooters<House>([new House(), new House()]);
+printHousesOrScooters<Scooter>([new Scooter(), new Scooter()]);
