437db2eb103345892750c51cea107da898850b06
matd <slyatbest@gmail.com>
2022-02-07 13:53:42 +0000

    Completed section 13.  Implementing a web framework.

---
+++ web/src/index.ts
@@ -0,0 +1,30 @@
+// import { UserEdit } from './views/UserEdit';
+// import { User } from './models/User';
+
+// const user = User.buildUser({ name: 'NAME', age: 20 });
+
+// const root = document.getElementById('root');
+
+// if (root) {
+//   const userEdit = new UserEdit(root, user);
+
+//   userEdit.render();
+
+//   console.log(userEdit);
+// } else {
+//   throw new Error('Root element not found');
+// }
+
+import { UserList } from './views/UserList';
+import { User } from './models/User';
+
+const users = User.buildUserCollection();
+
+users.on('change', () => {
+  const root = document.getElementById('root');
+  if (root) {
+    new UserList(root, users).render();
+  }
+});
+
+users.fetch();
