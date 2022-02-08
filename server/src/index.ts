2eb8f42ebed2fc3f5050a71df1346225fd52a001
matd <slyatbest@gmail.com>
2022-02-08 21:55:12 +0000

    Finished section 16.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,15 @@
 import express from 'express';
-import { router } from './routes/loginRoutes';
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
+import './controllers/LoginController';
+import './controllers/RootController';
+import { AppRouter } from './AppRouter';
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['laskdjf'] }));
-app.use(router);
+app.use(AppRouter.instance);
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
